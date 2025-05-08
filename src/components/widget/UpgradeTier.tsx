9fa8fd467310f4e82af1c3bdc394223b6794cd7c
unknown <lil>
2025-05-09 01:43:27 +0500

    pirce tier

--- src/components/widget/UpgradeTier.tsx
+++ src/components/widget/UpgradeTier.tsx
@@ -1,29 +1,36 @@
+"use client";
 import { CheckIcon } from '@heroicons/react/20/solid'
-import { Button } from '../ui/button'
+import { useRouter } from 'next/navigation'
+
 
 const tiers = [
   {
-    name: 'Hobby',
-    id: 'tier-hobby',
+    name: 'Monthly',
+    id: 'tier-monthly',
     href: '#',
-    priceMonthly: '$29',
+    duration: "month",
+    priceMonthly: '$5.99',
     description: "The perfect plan if you're just getting started with our product.",
-    features: ['25 products', 'Up to 10,000 subscribers', 'Advanced analytics', '24-hour support response time'],
+    features: ['10 forms', 
+      'Advanced analytics', 
+      '24-hour support response time',
+      'Early Access on few new features',
+    ],
     featured: false,
   },
   {
-    name: 'Enterprise',
-    id: 'tier-enterprise',
+    name: 'Yearly',
+    id: 'tier-yearly',
     href: '#',
-    priceMonthly: '$99',
+    priceMonthly: '$45',
+    duration: "year",
     description: 'Dedicated support and infrastructure for your company.',
     features: [
       'Unlimited products',
-      'Unlimited subscribers',
       'Advanced analytics',
+      'Newsletter feature',
       'Dedicated support representative',
-      'Marketing automations',
-      'Custom integrations',
+      'Early Access to all new features',
     ],
     featured: true,
   },
@@ -34,6 +41,7 @@ function classNames(...classes: any[]) {
 }
 
 export default function UpgradeTier() {
+  const router = useRouter();
   return (
     <div className="relative isolate bg-white px-6 py-10 lg:px-8">
       <div aria-hidden="true" className="absolute inset-x-0 -top-3 -z-10 transform-gpu overflow-hidden px-36 blur-3xl">
@@ -46,12 +54,12 @@ export default function UpgradeTier() {
         />
       </div>
       <div className="mx-auto max-w-4xl text-center">
-        <h2 className="text-base/7 font-semibold text-indigo-600">Pricing</h2>
+        <h2 className="text-base/7 font-semibold text-primary">Pricing</h2>
         <p className="mt-2 text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-6xl">
           Choose the right plan for you
         </p>
       </div>
-      <p className="mx-auto mt-6 max-w-2xl text-center text-lg font-medium text-pretty text-gray-600 sm:text-xl/8">
+      <p className="mx-auto mt-6 max-w-2xl text-center text-lg font-medium text-pretty text-muted-foreground sm:text-xl/8">
         Choose an affordable plan that’s packed with the best features for engaging your audience, creating customer
         loyalty, and driving sales.
       </p>
@@ -60,7 +68,7 @@ export default function UpgradeTier() {
           <div
             key={tier.id}
             className={classNames(
-              tier.featured ? 'relative bg-gray-900 shadow-2xl' : 'bg-white/60 sm:mx-8 lg:mx-0',
+              tier.featured ? 'relative bg-primary shadow-2xl' : 'bg-secondary sm:mx-8 lg:mx-0',
               tier.featured
                 ? ''
                 : tierIdx === 0
@@ -71,28 +79,30 @@ export default function UpgradeTier() {
           >
             <h3
               id={tier.id}
-              className={classNames(tier.featured ? 'text-indigo-400' : 'text-indigo-600', 'text-base/7 font-semibold')}
+              className={classNames(tier.featured ? 'text-muted' : 'text-muted-foreground', 'text-base/7 font-semibold')}
             >
               {tier.name}
             </h3>
             <p className="mt-4 flex items-baseline gap-x-2">
               <span
                 className={classNames(
-                  tier.featured ? 'text-white' : 'text-gray-900',
+                  tier.featured ? 'text-muted' : 'text-muted-foreground',
                   'text-5xl font-semibold tracking-tight',
                 )}
               >
                 {tier.priceMonthly}
               </span>
-              <span className={classNames(tier.featured ? 'text-gray-400' : 'text-gray-500', 'text-base')}>/month</span>
+              <span className={classNames(tier.featured ? 'text-muted' : 'text-muted-foreground', 'text-base')}>
+                /{tier.duration}
+                </span>
             </p>
-            <p className={classNames(tier.featured ? 'text-gray-300' : 'text-gray-600', 'mt-6 text-base/7')}>
+            <p className={classNames(tier.featured ? 'text-muted' : 'text-muted-foreground', 'mt-6 text-base/7')}>
               {tier.description}
             </p>
             <ul
               role="list"
               className={classNames(
-                tier.featured ? 'text-gray-300' : 'text-gray-600',
+                tier.featured ? 'text-muted' : 'text-muted-foreground',
                 'mt-8 space-y-3 text-sm/6 sm:mt-10',
               )}
             >
@@ -107,13 +117,13 @@ export default function UpgradeTier() {
               ))}
             </ul>
             <div
-            //   href={tier.href}
+            onClick={() => router.push(tier.href)}
               aria-describedby={tier.id}
               className={classNames(
                 tier.featured
-                  ? 'bg-indigo-500 text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-indigo-500'
-                  : 'text-indigo-600 ring-1 ring-indigo-200 ring-inset hover:ring-indigo-300 focus-visible:outline-indigo-600',
-                'mt-8 block rounded-md px-3.5 py-2.5 text-center text-sm font-semibold focus-visible:outline-2 focus-visible:outline-offset-2 sm:mt-10',
+                  ? 'bg-secondary text-primary shadow-xs hover:bg-secondary/80 focus-visible:outline-secondary/60'
+                  : 'bg-primary text-secondary ring-1 ring-primary/50 ring-inset hover:ring-primary/60 focus-visible:outline-primary/75',
+                'mt-8 block rounded-md px-3.5 py-2.5 text-center text-sm font-semibold focus-visible:outline-2 focus-visible:outline-offset-2 sm:mt-10 hover:cursor-pointer transition-all duration-500',
               )}
             >
               Get started today
